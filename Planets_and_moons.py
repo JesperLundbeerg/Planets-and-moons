70de96a5a2e304cc6f76e88a1e423ded5fac1243
Jesper Lundberg <Jeppsson@fam-lundberg.se>
2022-03-09 14:16:38 +0100

    Added bonus assignment

--- Planets_and_moons.py
+++ Planets_and_moons.py
@@ -11,7 +11,7 @@ class Planet:
     def __init__(self, name, size):
         self.name = name
         self.size = size
-        self.moons = ()
+        self.moons = []
 
     def __str__(self) -> str:
         return f"Name: {self.name}\nSize: {self.size}\nMoons: {self.moons}"
@@ -27,6 +27,16 @@ class Planet:
 
     def add_moon(self, moon):
         self.moons.append(moon)
+
+    def print_moons(self):
+        for moon in self.moons:
+            print(moon.get_name())
+
+    def certain_size(self, size : int):
+        print(f"The following moons around {self.get_name()} larger than {size} sqkm:")
+        for moon in self.moons:
+            if moon.get_size() > size:
+                print(moon.get_name())
 
 class Moon:
 
@@ -53,12 +63,6 @@ class Moon:
 
 
 def main():
-    pass
-
-
-
-if __name__ == "__main__":
-    main()
     tellus = Planet("Tellus", 5.101e8)
     luna = Moon("Luna", 3.8e7)
 
@@ -69,5 +73,27 @@ if __name__ == "__main__":
     jupiter = Planet("Jupiter", 6.142e10)
     europa = Moon("Europa", 3.09e7)
     io = Moon("Io", 4.191e7)
+
+    tellus.add_moon(luna)
+    mars.add_moon(phobos)
+    mars.add_moon(deimos)
+    jupiter.add_moon(europa)
+    jupiter.add_moon(io)
+
+    luna.add_orbit(tellus)
+    phobos.add_orbit(mars)
+    deimos.add_orbit(mars)
+    europa.add_orbit(jupiter)
+    io.add_orbit(jupiter)
+
+    tellus.print_moons()
+    jupiter.print_moons()
+
+    mars.certain_size(1000)
+
+
+if __name__ == "__main__":
+    main()
+    
     
 
